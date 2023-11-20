7959441aa27080f75e19b97d0f349636bcf92c93
devsanthosh2022 <52159154+devsanthosh2022@users.noreply.github.com>
2023-11-20 14:44:03 +0530

    Update justified-text.py
    Exception : Paragraph can't contain very long words that cannot fit within the specified page width.

--- justified-text.py
+++ justified-text.py
@@ -1,24 +1,26 @@
 def justify_paragraph(paragraph, page_width):
     try:
-
+        # Attempt to convert page width to an integer
         try:
-            # Attempt to convert page width to an integer
             page_width = int(page_width)
         except ValueError:
             print("Error: Page width must be a valid integer.")
             return None
 
-
         # Check if the page width is a positive integer
-        if page_width < 0:
+        if page_width <= 0:
             raise ValueError("Page width must be a positive integer.")
         elif page_width == 0:
             raise ValueError("Page width cannot be zero.")
-        
+
         # Check if the paragraph is empty
         if not paragraph:
             print("Error: Paragraph cannot be empty.")
             return None
+
+        # Check if the paragraph contains very long words
+        if any(len(word) > page_width for word in paragraph.split()):
+            raise ValueError("Paragraph contains very long words that cannot fit within the specified page width.")
 
         # Split the paragraph into words
         words = paragraph.split()
@@ -70,7 +72,7 @@ def justify_paragraph(paragraph, page_width):
         return justified
 
     except ValueError as e:
-        # Handle invalid page width input
+        # Handle invalid input
         print(f"Error: {e}")
         return None
 
